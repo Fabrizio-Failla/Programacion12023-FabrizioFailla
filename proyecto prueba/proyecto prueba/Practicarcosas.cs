7ce876a348fd2288f22baea87d8e475e76e56d96
Fabrizio-Failla <fabriziofailla123@gmail.com>
2023-04-25 20:37:13 -0300

    commit 9

--- proyecto prueba/proyecto prueba/Practicarcosas.cs
+++ proyecto prueba/proyecto prueba/Practicarcosas.cs
@@ -11,3 +11,71 @@ Console.WriteLine(cantidadDeProductos.Average());
 //cantidadDeProductos.Add agrgar elementos
 //cantidadDeProductos.Capacity = 10;// es la cantidad de capacidad que queres
 //cantidadDeProductos[1]*/
+/*List<int> codig = new List<int>();
+List<string> nombres = new List<string>();
+int codigos = 1;
+while (codigos != 0 && codigos <= 10)
+{
+    Console.WriteLine("Ingrese código del producto (1 al 10, 0 para finalizar):");
+    codigos = int.Parse(Console.ReadLine());
+    codig.Add(codigos);
+    if (codigos != 0 && codigos <= 10)
+    {
+        Console.WriteLine("Ingrese nombre del producto:");
+        string? producto = Console.ReadLine();
+        Console.WriteLine("Ingrese precio:");
+        int precio = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese cantidad:");
+        int cantidad = int.Parse(Console.ReadLine());
+        nombres.Add(producto);
+        codig.Add(precio);
+        codig.Add(cantidad);   
+    }  
+}
+int valorMaximo = codig.Max();
+Console.WriteLine("El codigo del producto con mayor cantidad es: "+valorMaximo);
+*/
+/*
+//Retomando el ejercicio 1 ; al finalizar la carga de productos, se ingresan las ventas
+//que hubo por cada código, puede haber más de 1 venta por cada articulo. El ingreso finaliza
+//al colocar un código en 0.  Informar al final los que tienen una cantidad (o sea la cantidad
+//inicial , restando las ventas que tuvo) menor o igual a 0
+int codigo = 1;
+double valorMaximo = 0;
+int codigoMaximo = 0;
+double valortotal;
+List<int> codigos = new List<int>();
+
+while (codigo != 0 && codigo <= 10)
+{
+    Console.WriteLine("Ingrese código del producto (1 al 10, 0 para finalizar):");
+    codigo = int.Parse(Console.ReadLine());
+
+    if (codigo != 0 && codigo <= 10)
+    {
+        Console.WriteLine("Ingrese nombre del producto:");
+        string? producto = Console.ReadLine();
+        Console.WriteLine("Ingrese precio:");
+        double precio = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese cantidad:");
+        double cantidad = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese ventas que tuvo:");
+        double ventas = double.Parse(Console.ReadLine());
+        valortotal = cantidad - ventas;
+
+        if (valortotal <= 0)
+        {
+            codigos.Add(codigo);
+        }
+    }
+}
+
+if (codigos.Count > 0)
+{
+    Console.WriteLine("Los productos que tienen ventas menores a 10 son:");
+    foreach (int codigoProducto in codigos)
+    {
+        Console.WriteLine("Codigo "+codigoProducto);
+    }
+}
+*/
--- proyecto prueba/proyecto prueba/repaso2.cs
+++ proyecto prueba/proyecto prueba/repaso2.cs
@@ -1,18 +1,20 @@
-﻿/* Retomando el ejercicio anterior, a continuación se colocan los nombres para
+﻿/*
+// Retomando el ejercicio anterior, a continuación se colocan los nombres para
 // cada código de producto, (opcional si la carga se realiza del 1 al 10 sin pedir
 // el código o si se ingresa el código y luego la descripción del mismo) ; al finalizar
 // la carga de los 10 códigos indicar el nombre de los productos que tuvieron un total de
 // ventas menor a 10 unidades.
+using System.Timers;
 int codigo = 1;
-double valorMaximo = 0;
-int codigoMaximo = 0;
-double valortotal;
+List<string> nombres = new List<string>();
 while (codigo != 0 && codigo <= 10)
+
 {
     Console.WriteLine("Ingrese código del producto (1 al 10, 0 para finalizar):");
     codigo = int.Parse(Console.ReadLine());
     if (codigo != 0 && codigo <= 10)
     {
+
         Console.WriteLine("Ingrese nombre del producto:");
         string? producto = Console.ReadLine();
         Console.WriteLine("Ingrese precio:");
@@ -21,21 +23,30 @@ while (codigo != 0 && codigo <= 10)
         double cantidad = double.Parse(Console.ReadLine());
         Console.WriteLine("Ingrese ventas que tuvo:");
         double ventas = double.Parse(Console.ReadLine());
-        valortotal = cantidad - ventas;
-        if (valortotal <= 0)
+        if (ventas < 10)
         {
-            codigoMaximo = codigo;
+            nombres.Add(producto);
         }
+
+
     }
 }
-Console.WriteLine("El producto cuyas cantidades restantes fueron menores o igual a 0 fueron el codigo: " + codigoMaximo);
 
-
+if (nombres.Count > 0)
+{
+    Console.WriteLine("Los productos que tienen menos de 10 ventas son:");
+    foreach (string nombre in nombres)
+    {
+        Console.WriteLine(nombre);
+    }
+}
+*/
 /*
 //Retomando el ejercicio 1 ; al finalizar la carga de productos, se ingresan las ventas
 //que hubo por cada código, puede haber más de 1 venta por cada articulo. El ingreso finaliza
 //al colocar un código en 0.  Informar al final los que tienen una cantidad (o sea la cantidad
 //inicial , restando las ventas que tuvo) menor o igual a 0
+//Opcion 1 antes de saber de las lista
 int codigo=1;
 double valorMaximo = 0;
 int codigoMaximo = 0;
@@ -62,7 +73,46 @@ while (codigo != 0 && codigo <= 10)
     }
 }
 Console.WriteLine("El producto cuyas cantidades restantes fueron menores o igual a 0 fueron el codigo: "+codigoMaximo);
-*/
+//Opcion 2 despues de conocer lista
+int codigo = 1;
+double valorMaximo = 0;
+int codigoMaximo = 0;
+double valortotal;
+List<int> codigos = new List<int>();
+
+while (codigo != 0 && codigo <= 10)
+{
+    Console.WriteLine("Ingrese código del producto (1 al 10, 0 para finalizar):");
+    codigo = int.Parse(Console.ReadLine());
+
+    if (codigo != 0 && codigo <= 10)
+    {
+        Console.WriteLine("Ingrese nombre del producto:");
+        string? producto = Console.ReadLine();
+        Console.WriteLine("Ingrese precio:");
+        double precio = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese cantidad:");
+        double cantidad = double.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese ventas que tuvo:");
+        double ventas = double.Parse(Console.ReadLine());
+        valortotal = cantidad - ventas;
+
+        if (valortotal <= 0)
+        {
+            codigos.Add(codigo);
+        }
+    }
+}
+
+if (codigos.Count > 0)
+{
+    Console.WriteLine("Los productos que tienen ventas menores a 10 son:");
+    foreach (int codigoProducto in codigos)
+    {
+        Console.WriteLine("Codigo "+codigoProducto);
+    }
+}
+ */
 /*
 //EJERCICIO
 //1 DE REPASO2
@@ -98,27 +148,3 @@ Console.WriteLine("El producto con mayor cantidad de unidades ("+valorMaximo+") 
 //1- Se ingresa el código de producto (son 10 productos, codificados del 1 al 10),luego el
 //precio y la cantidad que del mismo.La carga finaliza cuando se ingresa un código = 0
 //; al terminar indicar el código de producto que mayor cantidad tiene.
-List<int> codig = new List<int>();
-List<string> nombres = new List<string>();
-int codigos = 1;
-while (codigos != 0 && codigos <= 10)
-{
-    Console.WriteLine("Ingrese código del producto (1 al 10, 0 para finalizar):");
-    codigos = int.Parse(Console.ReadLine());
-    codig.Add(codigos);
-    if (codigos != 0 && codigos <= 10)
-    {
-        Console.WriteLine("Ingrese nombre del producto:");
-        string? producto = Console.ReadLine();
-        Console.WriteLine("Ingrese precio:");
-        int precio = int.Parse(Console.ReadLine());
-        Console.WriteLine("Ingrese cantidad:");
-        int cantidad = int.Parse(Console.ReadLine());
-        nombres.Add(producto);
-        codig.Add(precio);
-        codig.Add(cantidad);   
-    }  
-}
-int valorMaximo = codig.Max();
-Console.WriteLine("El codigo del producto con mayor cantidad es: "+valorMaximo + nombres );
-
